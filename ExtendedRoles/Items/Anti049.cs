79c49a19b9f90c875b8b3795f33c5b83c3dbab95
Noobest1001 <99349599+Noobest1001@users.noreply.github.com>
2025-01-08 15:02:23 +0000

    Added it for real now

--- ExtendedRoles/Items/Anti049.cs
+++ ExtendedRoles/Items/Anti049.cs
@@ -18,16 +18,29 @@ public class Anti049 : CustomItem
     public override string Description {get; set;} = "A concentrated amount of Epinephrine that can counter the effect of cardiac arrest.";
     public override float Weight {get; set;} = 1f;
 
+    protected override void SubscribeEvents() {
+        PlayerEvents.UsingItem += OnUsingItem;
+    
+        base.SubscribeEvents();
+    }
+
+    protected override void UnsubscribeEvents() {
+        PlayerEvents.UsingItem -= OnUsingItem;
+
+        base.UnsubscribeEvents();
+    }
+
     public void OnUsingItem(UsingItemEventArgs ev)
     {
-        ev.IsAllowed = false
-        if(ev.Player.Role.Team != Team.SCP)
-        {
-            ev.IsAllowed = true;
-            ev.Player.EnableEffect(EffectType.Invigorated, 1 );
-            ev.Player.RemoveEffect(EffectType.CardiacArrest);
-        } 
+        if(Check(ev.Item)) return;
 
-        
+        ev.Player.EnableEffect(EffectType.Invigorated, 2);
+        ev.Player.DisableEffect(EffectType.CardiacArrest)
+    }
+
+    public void OnDropping(DroppingItemEventArgs ev)
+    {
+        if(!Check(ev.Item)) return;
+        ev.IsAllowed = false;
     }
 }
